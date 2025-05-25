92ae63ff76e1b7cec9f8ef55686d0591d7b9ae8b
Auri Gabriel <ririmello.a@gmail.com>
2025-05-25 15:20:15 -0300

    feat: change text style

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -12,7 +12,7 @@ class PlacesList extends StatelessWidget {
       return Center(
         child: Text(
           'No places added yet.',
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
             color: Theme.of(context).colorScheme.onSurface,
           ),
         ),
